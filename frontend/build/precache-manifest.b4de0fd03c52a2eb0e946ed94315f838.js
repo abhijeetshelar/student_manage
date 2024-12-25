788b68aa0c845c915cc0effd447769201799fe52
abhijeetshelar789 <abhijeetshelar789@gmail.com>
2024-12-25 22:52:13 +0530

    changes in fronted pakage.json build: CI=false react-scripts build

---
+++ frontend/build/precache-manifest.b4de0fd03c52a2eb0e946ed94315f838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0202f6c10b1ee44e4ed02d09070cb5a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4780cd9a2a6c27c11286",
+    "url": "/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "f8e13df04ac5bcaee956",
+    "url": "/static/css/main.95df6c15.chunk.css"
+  },
+  {
+    "revision": "4780cd9a2a6c27c11286",
+    "url": "/static/js/2.33c301ae.chunk.js"
+  },
+  {
+    "revision": "f8e13df04ac5bcaee956",
+    "url": "/static/js/main.dada877d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
